2ac31c006149c6fc347f5352b9d0cdc4d65dd06e
cabarovaysel <cabarovaysel1@gmail.com>
2025-12-02 13:28:20 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+//const link = document.getElementsByClassName("link");
+///const parag = document.getElementsByTagName("p")[0];
+// console.log(link[2]);
+//link[0].style.color = "red";
+//link[2].style.backgroundColor = "red";
+//link[2].style.borderRadius = "10px";
+//link[2].style.transform = "translate(100%)";
+
+//parag.style.color = "red";
+
+//todo.innerHTML = "<i>Bu strong elementdir</i>";// html taglarini bir basa isledir
+//todo.textContent = "<b>Bu strong elementdir</b>";  // tag ile bi yerde isledir
+//const todo = document.getElementById("h1");
+//const todo2 = document.getElementsByClassName("h1")[0]; // id e gore cagirma
+//const h1 = document.querySelector("div h1");
+//h1.innerHTML = "Deyisdi";-//h1.style.color = "red";
+
+//const todo = document.querySelector("div");
+//const deyer = todo.children[0];
+//console.log(deyer);
+
+//const todo = document.querySelector("li");
+
+// todo console.log(todo.parentElement.parentElement.parentElement);
+
+// *const todo = document.querySelector("li:last-child");
+
+//todo.nextElementSibling.nextElementSibling.nextElementSibling.style.color ="red";
+//todo.previousElementSibling.previousElementSibling.style.color = "yellow";
+
+// ! <a href="" target="_blank" class="link">Link</a>
+
+/*const link = document.createElement("a");
+
+link.textContent = "Link";
+link.href = "http://youtube.com";
+link.className = "link";
+link.target = "_blank";
+link.style.color = "red";
+link.style.fontSize = "30px";
+link.style.fontWeight = "800";
+
+console.log(link);
+
+todo.append(link);*/
+
+// ? const todo = document.querySelector("div");
+
+// todo.removeChild(todo.children[3]);
